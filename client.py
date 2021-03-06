0224e3f4622640a0b4eda29cacf87d82db88a93b
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-03-07 00:28:18 +0100

    start client.py and server.py; implement commands

---
+++ client.py
@@ -0,0 +1,79 @@
+import socket
+
+
+HTTP_COMMANDS = ('HEAD', 'GET', 'PUT', 'POST') # Possible HTTP commands for this implementation
+
+
+# Get URI (e.g. http://www.google.com)
+uri = input('URI (press enter for www.google.com): ')
+
+# Parse URI (e.g. www.google.com)
+if uri.startswith('http://'):
+    uri = uri[len('http://'):]
+
+# Use www.google.com as default URI
+if not uri:
+    uri = 'www.google.com'
+
+
+# Get port
+port = input('Port (press enter to use default): ')
+
+# Use default port if no port specified
+if not port:
+    port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create TCP socket with ipv4
+
+# Connect to server
+try:  
+    server = socket.gethostbyname(uri) # TODO: also possible that user wants to connect to given ipv4 adress
+except socket.gaierror:  
+    # this means could not resolve the host  
+    print ('There was an error resolving the host') 
+    exit()
+
+client.connect((server, port))
+
+
+def do_command(cmd):
+    if cmd not in HTTP_COMMANDS:
+        print('[INVALID COMMAND] Command not recognized')
+        exit()
+
+    if cmd == 'HEAD':
+        msg = 'HEAD / HTTP/1.1\r\n'
+        msg += 'Host:%s\r\n' % (uri)
+        msg += '\r\n'
+    elif cmd == 'GET':
+        msg = 'GET / HTTP/1.1\r\n'
+        msg += 'Host:%s\r\n' % (uri)
+        msg += '\r\n'
+    elif cmd == 'PUT':
+        put_text = input('PUT text: ')
+        msg = 'PUT / HTTP/1.1\r\n'
+        msg += 'Host:%s\r\n' % (uri)
+        msg += 'Content-Type: text/html\r\n'
+        msg += 'Content-Length: %s' % (len(put_text)) + '\r\n'
+        msg += put_text + '\r\n'
+        msg += '\r\n'
+    elif cmd == 'POST':
+        post_text = input('POST text: ')
+        msg = 'POST / HTTP/1.1\r\n'
+        msg += 'Host:%s\r\n' % (uri)
+        msg += 'Content-Type: text/html\r\n'
+        msg += 'Content-Length: %s' % (len(post_text)) + '\r\n'
+        msg += post_text + '\r\n'
+        msg += '\r\n'
+
+    client.send(msg.encode())
+
+
+while True:
+    # Get HTTP command
+    command = input('HTTP command: ')
+    
+    do_command(command)
+
+    response = client.recv(100) # TODO: dynamic receive bytes
+    print(response)
---
+++ server.py
@@ -0,0 +1,33 @@
+# NOTE: for the demo we have to explain the difference between HTTP/1.0 and HTTP/1.1,
+# in the assignment we use HTTP/1.1
+import socket
+
+SERVER = "192.168.1.31" # TODO: use socket library to get ipv4 instead of hardcoding
+PORT = 5050 # Just some port that's not used by the machine
+ADDR = (SERVER, PORT)
+
+HEADER = 64 # TODO: figure out what is the right size (bytes) for the received message
+FORMAT = 'utf-8' # Find different format when it comes to images
+DISCONNECT_MESSAGE = 'disconnect'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def start(): # TODO: this will only work for one client at a time -> use threading library to start different threads in parallel for multiple client connection
+    server.listen()
+    
+    while True:
+        conn, addr = server.accept() # blocking: waits until client connected
+        print('[NEW CONNECTION]', addr[0], 'connected.')
+        connected = True
+        while connected:
+            msg = conn.recv(HEADER).decode(FORMAT).rstrip() # rstrip: remove special characters
+            print('[MESSAGE]', msg)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            conn.send(bytes(connected)) # Needed to let client know if he's still connected
+        print('[CLOSE CONNECTION]', addr[0], 'disconnected')
+        conn.close()
+
+print('[STARTING] server is starting...')
+start()
