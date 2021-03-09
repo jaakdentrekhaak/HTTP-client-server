aa5fe105c972eb2cbb15cd647bba258ea2104af9
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-03-09 12:07:07 +0100

    implement beautifulsoup; add Q&A3.txt

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import re
+from bs4 import BeautifulSoup
 
 HTTP_COMMANDS = ('HEAD', 'GET', 'PUT', 'POST') # Possible HTTP commands for this implementation
 
@@ -35,6 +36,20 @@ except socket.gaierror:
 
 client.connect((server, port))
 
+def make_GET(path):
+    """Generate GET message with given path
+
+    Args:
+        path (string): path to request
+
+    Returns:
+        string: GET message
+    """
+    msg = f'GET {path} HTTP/1.1\r\n'
+    msg += f'Host:{uri}\r\n'
+    msg += '\r\n'
+
+    return msg
 
 def do_command(cmd):
     """Send HTTP request to the server depending on the type of command
@@ -51,9 +66,7 @@ def do_command(cmd):
         msg += f'Host:{uri}\r\n'
         msg += '\r\n'
     elif cmd == 'GET':
-        msg = 'GET / HTTP/1.1\r\n'
-        msg += f'Host:{uri}\r\n'
-        msg += '\r\n'
+        msg = make_GET('/')
     elif cmd == 'PUT':
         put_text = input('PUT text: ')
         msg = 'PUT / HTTP/1.1\r\n'
@@ -73,58 +86,124 @@ def do_command(cmd):
         msg += post_text + '\r\n'
         msg += '\r\n'
 
-    client.send(msg.encode())
+    client.send(msg.encode()) # Encode with default utf-8
 
-def store_html(resp):
-    """Search HTML body in the given response and store into HTML file
+def get_html_body(resp):
+    """Return html body from HTTP response
 
     Args:
-        resp (bytes): HTTP server response
+        resp (bytes): HTTP response
+
+    Returns:
+        string: HTML body
     """
     # Get HTML body
     # TODO: maybe use content-length and transfer-encoding?
-    start = resp.index('<!doctype html>')
-    end = resp.index('</html>') + len('</html>')
+    start = resp.index(b'<!doctype html>')
+    end = resp.index(b'</html>') + len(b'</html>')
     body = resp[start:end]
-    
+
+    return body
+
+def store_html(html):
+    """Store html into HTML file
+
+    Args:
+        html (BeautifulSoup): HTML
+    """
     # Store HTML body
     open('received.html', 'w').close() # Remove contents of html file
     file = open('received.html', 'w')
-    file.write(body)
+    file.write(html.prettify())
     file.close()
 
-def get_encoding(resp):
-    """Get encoding type of HTTP response
+def request_img(url):
+    """Request the image from the server and store it locally in the same relative path.
+    External urls should just be downloaded in a folder, but are not used.
 
     Args:
-        resp (bytes): HTTP server response
+        url (string): url/path of the image
+    """
+    ## Local images (GET to current server)
+    if url.startswith('/'):
+        msg = make_GET(url)
+        client.send(msg.encode())
+        resp = receive_response()
+        # TODO: continue
+
+
+    ## External images (open other socket to external server)
+
+
+def fix_html(html):
+    """Find missing images in the given html, request them from the server and
+    store them locally.
+
+    Images can be stored locally:
+        e.g. /images/nav_logo229.png
+    or loaded from external website:
+        e.g. https://ssl.gstatic.com/gb/images/b_8d5afc09.png
+    
+    Images will be stored in <img ... src="">
+
+    Args:
+        html (BeautifulSoup): HTML body
 
     Returns:
-        string: encoding type of given response
+        string: fixed HTML body
     """
-    # Using regex to search word following the given keyword
-    return re.search(b'charset=(\S+)', resp).group(1).decode('utf-8')
 
-while True:
+    imgs = html.findAll('img')
+
+    for img in imgs:
+        request_img(img['src'])
+
+
+    return html
+
+def receive_response():
+    """Receive response from server after request
+
+    Returns:
+        bytes: HTTP response from server
+    """
+    resp = b''
+    while True:
+        received = client.recv(1024)
+        resp += received
+        # TODO: use Content-Length and Transfer-Encoding: chunked
+        if received.endswith(b'\r\n\r\n'):
+            break
+    return resp
+
+def main():
     # Get HTTP command
     command = input('HTTP command: ')
-    
+
     do_command(command)
 
     # Get response from server
-    response = b''
-    while True:
-        received = client.recv(1024)
-        response += received
-        if received.endswith('\r\n\r\n'.encode('utf-8')):
-            break
-    
-    # Decode message
-    response = response.decode(get_encoding(response)) # E.g. 'ISO-8859-1' # TODO: get encoding with 'charset' in content-length header
+    response = receive_response()
 
     # Store HTML body in file
-    # TODO: download images in html file
     if command == 'GET':
-        store_html(response)
+        # Get HTML body
+        body = get_html_body(response)
 
-    print(response)
+        # Prettify body
+        soup = BeautifulSoup(body, 'html.parser')
+
+        # Store body
+        store_html(soup)
+
+        # # Fix html with images
+        # fix_html(soup)
+    
+    elif command == 'HEAD':
+        print(response.decode('utf-8'))
+
+    # Close connection
+    # TODO: send 'Connection: close' header to server
+    client.close()
+
+main()
