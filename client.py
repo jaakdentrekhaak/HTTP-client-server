c692a11d3fe131c06155ea899d90f4a2d4f1c5cc
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-03-25 21:29:23 +0100

    reorganize client.py: everything inside functions

--- client.py
+++ client.py
@@ -2,99 +2,57 @@ import socket
 from bs4 import BeautifulSoup
 import sys
 
+# Example websites
+# 
+# www.example.com
+# www.google.com
+# www.tcpipguide.com
+# www.jmarshall.com
+# www.tldp.org
+# www.tinyos.net
+# www.linux-ip.net
 
-HTTP_COMMANDS = ('HEAD', 'GET', 'PUT', 'POST') # Possible HTTP commands for this implementation
-
-# Check input arguments
-if len(sys.argv) != 4:
-    print('Input arguments must be: HTTP command; URI; Port')
-    exit()
-
-# Get URI (e.g. http://www.google.com)
-# print('Enter the ipv4 adress of a server, the link of a website or choose from the following options by entering the corresponding number.')
-# uri = input('1. example, 2. google, 3. tcpipguide, 4. jmarshall, 5. tldp, 6. tinyos, 7. linux-ip, 8. browserify (extra): ') or 'www.example.com'
-uri = sys.argv[2]
-
-# If number is entered -> map to website
-if uri == '1':
-    uri = 'www.example.com'
-elif uri == '2':
-    uri = 'www.google.com'
-elif uri == '3':
-    uri = 'www.tcpipguide.com'
-elif uri == '4':
-    uri = 'www.jmarshall.com'
-elif uri == '5':
-    uri = 'www.tldp.org'
-elif uri == '6':
-    uri = 'www.tinyos.net'
-elif uri == '7':
-    uri = 'www.linux-ip.net'
-elif uri == '8':
-    uri = 'browserify.org'
-
-# Parse URI (e.g. www.google.com)
-if uri.startswith('http://'):
-    uri = uri[len('http://'):]
-
-# Get port
-# port = input('Port (press enter for default port 80; own server uses port 5050): ') or 80
-port = sys.argv[3]
-port = int(port)
-
-# Create TCP socket with ipv4
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect to server
-if uri.startswith('192'): # possible that user wants to connect to given ipv4 adress
-        server = uri
-else:
-    try:
-        server = socket.gethostbyname(uri)
-    except socket.gaierror:  
-        # this means could not resolve the host  
-        print ('There was an error resolving the host') 
-        exit()
-client.connect((server, port))
-
-def create_get_message(path):
+def create_get_message(url, path):
     """Generate GET message with given path
 
     Args:
+        url (string): url of server
         path (string): path to request
 
     Returns:
         string: GET message
     """
     msg = f'GET {path} HTTP/1.1\r\n'
-    msg += f'Host: {uri}\r\n'
+    msg += f'Host: {url}\r\n'
+    # msg += 'If-Modified-Since: Sun, 14 Mar 2021 17:10:27 GMT\r\n'
     msg += '\r\n'
 
     return msg
 
-def do_command(cmd):
+def do_command(client, cmd, url, path):
     """Send HTTP request to the server depending on the type of command
 
     Args:
+        client (object): socket
         cmd (string): One of the possible HTTP commands
+        url (string): url of server
     """
-    if cmd not in HTTP_COMMANDS:
+    # Possible HTTP commands for this implementation
+    if cmd not in ('HEAD', 'GET', 'PUT', 'POST'):
         print('[INVALID COMMAND] Command not recognized')
         exit()
 
     if cmd == 'HEAD':
-        path = input('Enter path of the file you want request (press enter for default /): ') or '/'
         msg = f'HEAD {path} HTTP/1.1\r\n'
-        msg += f'Host: {uri}\r\n'
+        msg += f'Host: {url}\r\n'
+        # msg += 'If-Modified-Since: Sun, 14 Mar 2021 17:10:27 GMT\r\n'
         msg += '\r\n'
     elif cmd == 'GET':
-        path = input('Enter path of the file you want request (press enter for default /): ') or '/'
-        msg = create_get_message(path)
+        msg = create_get_message(url, path)
     elif cmd == 'PUT' or cmd == 'POST':
-        path = input('Enter path of the file you want to write to (press enter for default /test.txt): ') or '/test.txt'
         text = input('Enter text you want to send to the server: ')
         msg = f'{cmd} {path} HTTP/1.1\r\n'
-        msg += f'Host: {uri}\r\n'
+        msg += f'Host: {url}\r\n'
         msg += 'Content-Type: text/html\r\n'
         length = len(text) + len(b'\r\n\r\n')
         msg += f'Content-Length: {length}\r\n'
@@ -127,11 +85,11 @@ def store_img(img, path):
     file.write(img)
     file.close()
 
-def get_external_image(website, path):
+def get_external_image(client, website, path):
     """Open new socket to website, send GET for external image and return server response with image
 
     Args:
-        website (string): name of the website
+        website (string): name of external server
         path (string): relative path of the external image
 
     Returns:
@@ -140,36 +98,41 @@ def get_external_image(website, path):
     cl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sv = socket.gethostbyname(website)
     cl.connect((sv, 80))
-    cl.send(create_get_message(path).encode())
-    resp = handle_response()
+    cl.send(create_get_message(website, path).encode())
+    headers = get_headers(client)
+    resp = handle_response(client, headers)
     cl.close()
     return resp
 
-def request_img(url):
+def request_img(client, uri, url):
     """Request the image from the server and store it locally in the same relative path.
     External urls should just be downloaded in a folder, but are not used.
 
     Args:
-        url (string): url/path of the image
+        client (object): socket
+        uri (string): url/path of the image
+        url (string): url of server connected to
     """
     ## Local images (GET to current server)
-    if not url.startswith('http'):
-        if not url.startswith('/'):
-            url = '/' + url 
-        msg = create_get_message(url)
+    if not uri.startswith('http'):
+        if not uri.startswith('/'):
+            uri = '/' + uri 
+        print('[REQUEST IMG]', url, uri)
+        msg = create_get_message(url, uri)
         client.send(msg.encode())
-        resp = handle_response()
-        store_img(resp, url)
+        headers = get_headers(client)
+        resp = handle_response(client, headers)
+        store_img(resp, uri)
 
     ## External images (open other socket to external server)
     else:
         # E.g. https://ssl.gstatic.com/gb/images/b_8d5afc09.png
-        split_url = url.split('/')
-        website = split_url[2]
-        img_path = '/' + '/'.join(split_url[3:])
-        store_img(get_external_image(website, img_path), img_path)
+        split_uri = uri.split('/')
+        website = split_uri[2]
+        img_path = '/' + '/'.join(split_uri[3:])
+        store_img(get_external_image(client, website, img_path), img_path)
 
-def fix_html(html):
+def fix_html(client, html, url):
     """Find missing images in the given html, request them from the server and
     store them locally.
 
@@ -181,13 +144,15 @@ def fix_html(html):
     Images will be stored in <img ... src="">
 
     Args:
+        client (object): socket
         html (BeautifulSoup): HTML body
+        url (string): url of connected server
     """
 
     imgs = html.findAll('img')
 
     for img in imgs:
-        request_img(img['src'])
+        request_img(client, img['src'], url)
         # Change src to local images folder (e.g. images/yeet.png)
         image_name = img['src'].split('/')[-1] # Grab image name from path
         img['src'] = 'images/' + image_name
@@ -211,7 +176,7 @@ def get_content_length(headers):
     ind = after_keyword.index(b'\r')
     return int(after_keyword[:ind])
 
-def get_next_chunk():
+def get_next_chunk(client):
     """Read the size of the chunk and receive body of chunk
 
     Returns:
@@ -236,7 +201,7 @@ def get_next_chunk():
     
     return body
 
-def get_headers():
+def get_headers(client):
     """Extract the headers from the HTTP server response
 
     Returns:
@@ -251,15 +216,18 @@ def get_headers():
     
     return headers
 
-def handle_response():
+def handle_response(client, headers):
     """Receive HTTP response from server and return response body
+
+    Args:
+        client (object): socket
+        headers (bytes): headers of the HTTP response
 
     Returns:
         bytes: HTTP response body
     """
-    
-    # First get headers: read byte per byte until we have the headers
-    headers = get_headers()
+
+    # TODO: update documentation (new arguments)
     
     # See if we need to use content-length or transfer-encoding
     if b'Content-Length' in headers:
@@ -273,43 +241,85 @@ def handle_response():
             body += client.recv(buff_size)
     else:
         body = b''
-        chunk = get_next_chunk()
+        chunk = get_next_chunk(client)
         while len(chunk) != 0:
             body += chunk
-            chunk = get_next_chunk()
-    
+            chunk = get_next_chunk(client)
+
     return body
 
-def main():
-    # Get HTTP command
-    # command = input('HTTP command (HEAD, GET, POST or PUT): ')
-    command = sys.argv[1]
+def main(command, uri, port):
+    """Run the code
 
-    do_command(command)
+    Args:
+        command (string): HTTP command
+        uri (string): path to file on server
+        port (string): port to connect to
+    """
 
+    # Get url of given uri and the path of the file
+    url = uri.split('/')[0]
+    path = '/'.join(uri.split('/')[1:]) or '/' # Default is /
+
+    # Parse URI (e.g. www.google.com)
+    if url.startswith('http://'):
+        url = url[len('http://'):]
+
+    port = int(port)
+
+    # Create TCP socket with ipv4
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to server
+    if url == 'localhost':
+        print('Connect to your own server with your ipv4 address.')
+        exit()
+    elif url.startswith('192'): # possible that user wants to connect to given ipv4 adress
+            server = url
+    else:
+        try:
+            server = socket.gethostbyname(url)
+        except socket.gaierror:  
+            # this means could not resolve the host  
+            print ('There was an error resolving the host') 
+            exit()
+    client.connect((server, port))
+
+    # Execute command
+    do_command(client, command, url, path)
+
+    # Get header from response
+    headers = get_headers(client)
+
+    # If not 200 OK in headers -> just show headers in terminal
+    if b'200 OK' not in headers:
+        print(headers.decode('utf-8'))
 
     # Store HTML body in file
-    if command == 'GET' or command == 'POST' or command == 'PUT':
+    elif command == 'GET' or command == 'POST' or command == 'PUT':
         # Get HTML body
-        body = handle_response()
+        body = handle_response(client, headers)
 
         # Prettify body
         soup = BeautifulSoup(body, 'lxml')
 
         # Fix html with images
-        fix_html(soup)
+        fix_html(client, soup, url)
 
         print('[INFO] Request succeeded, the response can be viewed in received.html')
     
     # Print headers
     elif command == 'HEAD':
-        # Get response from server
-        response = get_headers()
-
-        print(response.decode('utf-8')) # Decoding for readability in terminal
+        print(headers.decode('utf-8')) # Decoding for readability in terminal
 
     # Close connection
     # TODO (optional): send 'Connection: close' header to server
     client.close()
 
-main()
+if __name__ == '__main__':
+    # Check input arguments
+    if len(sys.argv) != 4:
+        print('Input arguments must be: client.py; HTTP command; URI; Port')
+        exit()
+    command, uri, port = sys.argv[1:]
+    main(command, uri, port)
